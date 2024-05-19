ccd7c98852c4e2a49a326d5159b852681caaed2a
amanikitutocd.. <akituto@gmail.com>
2024-05-20 01:12:32 +0300

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python3
-
+# lib/dog.py
 class Dog:
-    # Class body goes here
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+
+# lib/person.py
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
